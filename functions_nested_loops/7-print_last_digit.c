e120e48322a777a51cd9262096e9f09c92d7009c
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-06 13:27:15 -0700

    print the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,8 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (n < 0)
-		n = n * (-1);
+		n *= -1;
 
-	_putchar('0' + last_digit);
-	_putchar('\n');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
