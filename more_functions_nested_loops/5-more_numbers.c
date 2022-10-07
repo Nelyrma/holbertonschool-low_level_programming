16dbd4d4830cb4eba97c9261e6c460ec67f19390
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-07 10:28:50 -0700

    print numbers 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char n;
+
+	for (i = 0; i < 10, i++)
+	{
+		for (n >= '0'; n <= '14'; n++)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
