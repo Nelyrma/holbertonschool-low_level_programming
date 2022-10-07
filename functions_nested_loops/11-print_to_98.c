6f21b0bef57ca875d580a9a451c4dcd28059cc3d
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-07 01:13:11 -0700

    print numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,18 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
-		printf("%d", n);
-		_putchar(',');
-		_putchar('\n');
+		printf("%d, ", n);
 		n++;
 	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d, ", n);
+
+	_putchar('\n');
 }
