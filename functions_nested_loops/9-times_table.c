c53747d2b9e83252f63edb59c3dfd4854607feb5
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-08 03:27:40 -0700

    the 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,8 +12,7 @@ void times_table(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar('0');
-
+	
 		for (m = 0; m <= 9; m++)
 		{
 			_putchar(',');
@@ -22,6 +21,7 @@ void times_table(void)
 
 			if (p <= 9)
 			{
+				_putchar('0' + p);
 				_putchar(' ');
 			}
 			else
