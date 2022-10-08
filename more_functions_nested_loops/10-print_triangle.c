510a4501fe55065de825fa1989d88541ef52fbf7
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-08 11:39:10 -0700

    draw a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,11 +1,27 @@
 #include "main.h"
 
 /**
- * print_triangle - print a triangle
- * @size : the variable
+ * print_triangle - function that prints triangle
+ * @size: the variable
  */
 
 void print_triangle(int size)
 {
-	
+	int height;
+	int width;
+	int triangle;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 0; height <= size; height++)
+	{
+		for (width =  1; width <= (size - height); width++)
+			_putchar(' ');
+
+		for (triangle = 1; triangle <= height; triangle++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
 }
