32dd799c5619bbb2e9d89dc69d0d3421a0affeb9
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-11 01:40:52 -0700

    print half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: the variable
+ * Return: the value of the variable length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+
+/**
+ * puts_half - print half of a string
+ * @str : the variable
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(s);
+	int i;
+
+	if ((len % 2) != 0)
+		i = (len / 2) + 1;
+	else
+		i = (len / 2);
+
+	while (i < len)
+	{
+		_putchar(*str + 1);
+		i++;
+	}
+}
