cd05b414d112b24be34a942b692b4c4a243b7067
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-12 02:55:11 -0700

    copy the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ *_strlen - return the length of a string
+ * @s: the variable
+ * Return: the value of the variable length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * *_strcpy - copy the string pointed
+ * @dest: the destination of the string
+ * @src: the destination of the string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n = _strlen(char src);
+
+	for (i = 0; i < n ; i++)
+		dest[i] = src[i];
+
+	return (dest[i]);
+}
