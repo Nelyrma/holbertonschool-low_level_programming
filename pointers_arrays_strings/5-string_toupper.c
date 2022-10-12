9ed079ee296081c003c75fb96b4283b2b30368c7
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-12 13:59:05 -0700

    lowercase to uppercase letters

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,10 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	while (s[i]  != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
-		i++;
 	}
 	return (s);
 }
