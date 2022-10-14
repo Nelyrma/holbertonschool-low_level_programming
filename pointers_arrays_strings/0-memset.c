c13f1b49b178114821e36494a869efcb7521b2bc
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-14 02:47:48 -0700

    fill memory with contant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - fill memory with constant byte
+ * @s : the memory space to be filled
+ * @b: the byte to fill with
+ * @n: number of space to fill
+ * Return: the pointer to memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0, i < n, i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
