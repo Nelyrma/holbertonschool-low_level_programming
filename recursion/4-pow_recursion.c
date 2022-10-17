464e7508b964338a80a84d66a443a4d9e5ea547d
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-17 09:18:17 -0700

    x raised to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x : the variable to be raised
+ * @y : the power
+ * Return: the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x, y));
+}
