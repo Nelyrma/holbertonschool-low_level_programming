a5ee4439ed4d4a2e12ca5faaaa5857d7226d95d8
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-17 09:56:01 -0700

    x raised to the power of y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,9 +13,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y == 0)
+	if (x == 1 || y == 0)
 	{
 		return (1);
 	}
-	return (_pow_recursion(x, y));
+	return (x * _pow_recursion(x, y - 1));
 }
