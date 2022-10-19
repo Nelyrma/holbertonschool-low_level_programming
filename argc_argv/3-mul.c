f3555dc834be2770f692384cc0f4793064a20951
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-19 13:55:41 -0700

    multiplication of two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = a * b;
+	printf("%d\n", mul);
+	return (0);
+
+}
