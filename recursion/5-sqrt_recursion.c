049fbbd3454bd0ad9976d8cd44d3e5b1a98a1255
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-20 11:26:43 -0700

    natural square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include <stio.h>
+
+/**
+ * square_root - check the natural square root of a number
+ * @n : the number
+ * Return: carree if n have natural square root, -1 if not
+ */
+
+int square_root(int n)
+{
+	int a;
+
+	if (a * a > n)
+		return (-1);
+
+	if (a * a == n)
+		return (square_root(a, n  + 1));
+}
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: the number
+ * Return: -1 if n does not have a natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	printf(square_root(n, 0));
+}
