170392a44162460bfdc4f30bccdd6496c1af43d3
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-10-25 02:42:50 -0700

    free a 2 dimensional grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free a 2 dimensional grid created previously
+ * @grid: the array
+ * @height: the height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == O)
+		free(grid);
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
