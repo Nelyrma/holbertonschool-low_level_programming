435fadd5bdacda804dbf9dd8385b6c628a5f678c
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-02 02:22:56 -0700

    print name

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: char string
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
