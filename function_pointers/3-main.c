2071442637791e17dc1228bf0a78930b38c30d98
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-03 03:41:45 -0700

    simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (f == NULL || argv[2][0] != '\0')
+	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
