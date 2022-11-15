eccf22c42ca2c6d66e1a8f4c9a3aa0ce23e73bf8
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-15 14:33:49 -0800

    Print the elements of a list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t size = 0;
 
-	if (h != NULL)
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
-
-	size++;
-	h = h->next;
-
+		size++;
+		h = h->next;
+	}
 	return (size);
 }
