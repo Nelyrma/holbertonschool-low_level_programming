b7a4210670cab943f3d659ec6a73ae1aeadbe3d4
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-16 06:39:12 -0800

    Get the nth node of a linked list

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (current != NULL)
 	{
 		if (i == index)
-			return (current);
+			return (current->n);
 		i++;
 		current = current->next;
 	}
