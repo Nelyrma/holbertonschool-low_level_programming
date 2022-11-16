6e60880ebcd33d55a55d53b6e12373c0ea1df8be
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-16 01:14:59 -0800

    Free e linked list

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	while (*head != NULL)
+	if (*head != NULL)
 	{
-		node = *head;
-		*head = (*head)->next;
-		free(node);
+		while (*head != NULL)
+		{
+			node = *head;
+			*head = (*head)->next;
+			free(node);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
