e2c59cb9a295ad79fd561e9eb7d0429e87af86ce
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-16 01:11:36 -0800

    Free a linked list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list
+ * @head: the list to free
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head != NULL)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+	*head = NULL;
+}
