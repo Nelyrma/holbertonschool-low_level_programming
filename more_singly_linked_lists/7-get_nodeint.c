4bdc65aa25f924ca9f451fb44a19386fc933c0ef
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-17 01:10:25 -0800

    The nth node of a linked list

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	if (i < index && head != NULL)
+	while (i < index && head != NULL)
 	{
 		head = head->next;
 		i++;
 	}
+
 	if (i == index)
 		return (head);
+
+	return (NULL);
 }
