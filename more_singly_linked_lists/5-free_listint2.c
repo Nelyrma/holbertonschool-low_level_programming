cc2f11c29b1fe942a88a7386a42b5d43b4d71178
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-17 00:39:56 -0800

    Free a linked list

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
 		node = *head;
 		*head = node->next;
-		free(node->n);
 		free(node);
 	}
 	*head = NULL;
