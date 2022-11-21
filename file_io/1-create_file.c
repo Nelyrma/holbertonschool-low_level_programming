90799efd55eef47b9074e0490a4ddac2ab745407
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-21 05:29:55 -0800

    Create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,11 +16,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_CREAT | 0_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
-		clode(fd);
+		close(fd);
 		return (-1);
 	}
 
