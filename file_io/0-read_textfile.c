de1590f8f85e3984102df1fc68d12f776c03e8e0
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-21 04:42:29 -0800

    Read a text file and print it

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	write_c = write(fd, buf, letters);
+	write_c = write(STDOUT_FILENO, buf, read_c);
 	if (write_c == -1)
 	{
 		close(fd);
