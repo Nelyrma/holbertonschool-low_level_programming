511dde783e9e4ee4698770476704c19a0e2b7826
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-21 05:36:28 -0800

    Create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0', i++)
+		for (i = 0; text_content[i] != '\0'; i++)
 			;
 
 		write_c = write(fd, text_content, i);
