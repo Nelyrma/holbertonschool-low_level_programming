8cfba87944f365b0ef3bdef4859a267cab0505dc
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-21 05:23:10 -0800

    Create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: pointer to the address of the filename
+ * @text_content: the content of the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int write_c;
+	int fd;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, 0_CREAT | 0_WRONLY, 0600);
+
+	if (fd == -1)
+	{
+		clode(fd);
+		return (-1);
+	}
+
+	if (text_context != NULL)
+	{
+		for (i = 0; text_content[i] != '\0', i++)
+			;
+		write_c = write(fd, text_content, i);
+
+		if (write_c == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+
+		close(fd);
+		return (1);
+	}
+
+
+
+}
