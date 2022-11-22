0e17628c59bb244aaa93f9ffbfce45768115c2d7
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-22 13:44:58 -0800

    Add a new node at the beginning of a list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		while ((*head)->prev != NULL)
 			*head = (*head)->prev;
-		(*head)->prev = new;
+		(*head)->prev = new_node;
 	}
 
 	new_node->n = n;
