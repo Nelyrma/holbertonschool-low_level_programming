990716b8d75d8707e0845f79a70360c86c26e705
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-23 06:52:22 -0800

    Sum ofall data

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - return the sum of all data (n)
+ * @head: pointer to the head node
+ * Return: the sum of all data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+
+	sum = 0
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
