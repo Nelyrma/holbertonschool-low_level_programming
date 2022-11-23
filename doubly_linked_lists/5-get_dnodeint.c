746484db8bef393b62546f995b47232f5d8043b2
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-23 02:26:44 -0800

    Return the nth node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,9 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	for (i = 0; i < index; i++)
-	{
 		head = head->next;
-	}
 
 	if (i == index)
 		return (head);
